eca633035cfd1fff951155a2052a8f8e974661e9
Josh Hughes <stalak@gmail.com>
2023-03-31 20:35:05 +0100

    Upgrade to .NET 7. Move endpoints to dedicated file Add rate limiting

---
+++ Rapture.Api/Endpoints.cs
@@ -0,0 +1,25 @@
+﻿using Rapture.Common.Mapper;
+using Rapture.Service;
+
+namespace Rapture.Api;
+
+internal static class Endpoints
+{
+    internal static void LoadEndpoints(this WebApplication app)
+    {
+        app.MapGet("/GetAllRaptures", async (IRaptureService service) =>
+            RaptureToRaptureDetailsViewModel.Map(await service.GetAllRaptures()));
+
+        app.MapGet("/GetSurvivedRaptures", async (IRaptureService service, DateTime dateOfBirth) =>
+            RaptureToRaptureDetailsViewModel.Map(await service.GetSurvivedRaptures(dateOfBirth)));
+
+        app.MapGet("/GetNextRapture", async (IRaptureService service) =>
+            RaptureToRaptureDetailsViewModel.Map(await service.GetNextRapture()));
+
+        app.MapGet("/GetCurrentRapture", async (IRaptureService service) =>
+            RaptureToRaptureDetailsViewModel.Map(await service.GetCurrentRapture()));
+
+        app.MapGet("/GetAllFutureRaptures", async (IRaptureService service) =>
+            RaptureToRaptureDetailsViewModel.Map(await service.GetAllFutureRaptures()));
+    }
+}
--- Rapture.Api/Program.cs
+++ Rapture.Api/Program.cs
@@ -1,6 +1,7 @@
-using Rapture.Common.Mapper;
+using Rapture.Api;
 using Rapture.Repository;
 using Rapture.Service;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,13 +13,24 @@ builder.Services.AddScoped<IRaptureRepository, RaptureRepository>();
 builder.Services.AddScoped<IRaptureService, RaptureService>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() { Title = "Rapture.Api", Version = "v1" });
-});
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new() { Title = "Rapture.Api", Version = "v1" }));
 
-builder.Services.AddCors(options => 
+builder.Services.AddCors(options =>
     options.AddPolicy("joshh", policy => policy.WithOrigins("https://www.joshh.co.uk/")));
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            context.Request.Host.ToString(),
+            _ => new FixedWindowRateLimiterOptions
+            {
+                AutoReplenishment = true,
+                PermitLimit = 60,
+                QueueLimit = 0,
+                Window = TimeSpan.FromHours(1)
+            }));
+});
 
 var app = builder.Build();
 
@@ -29,20 +41,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rapture.Api v1"));
 }
 
-// Endpoints
-app.MapGet("/GetAllRaptures", async (IRaptureService service) => 
-    RaptureToRaptureDetailsViewModel.Map(await service.GetAllRaptures()));
-
-app.MapGet("/GetSurvivedRaptures", async (IRaptureService service, DateTime dateOfBirth) => 
-    RaptureToRaptureDetailsViewModel.Map(await service.GetSurvivedRaptures(dateOfBirth)));
-
-app.MapGet("/GetNextRapture", async (IRaptureService service) => 
-    RaptureToRaptureDetailsViewModel.Map(await service.GetNextRapture()));
-
-app.MapGet("/GetCurrentRapture", async (IRaptureService service) => 
-    RaptureToRaptureDetailsViewModel.Map(await service.GetCurrentRapture()));
-
-app.MapGet("/GetAllFutureRaptures", async (IRaptureService service) => 
-    RaptureToRaptureDetailsViewModel.Map(await service.GetAllFutureRaptures()));
+app.LoadEndpoints();
 
 app.Run();
